9844ebbb4a443add25d896b924291fa6e43ba6f9
cryotux <323978812+cryotux@users.noreply.github.com>
2026-09-02 09:50:09 +0000

    Fixed iamchroot.sh

--- src/iamchroot.sh
+++ src/iamchroot.sh
@@ -12,61 +12,104 @@
 # (at your option) any later version.
 #
 # artix-installer is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-# General Public License for more details.
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+# for more details.
 #
 # You should have received a copy of the GNU General Public License
 # along with artix-installer. If not, see <https://www.gnu.org/licenses/>.
 
-# Boring stuff you should probably do
-ln -sf /usr/share/zoneinfo/"$REGION_CITY" /etc/localtime
+# Timezone
+ln -sf "/usr/share/zoneinfo/$REGION_CITY" /etc/localtime
 hwclock --systohc
 
 # Localization
-printf "%s.UTF-8 UTF-8\n" "$LANGCODE" >>/etc/locale.gen
+if ! grep -q "^#*$LANGCODE\.UTF-8 UTF-8" /etc/locale.gen; then
+	printf "%s.UTF-8 UTF-8\n" "$LANGCODE" >>/etc/locale.gen
+fi
+
 locale-gen
+
 printf "LANG=%s.UTF-8\n" "$LANGCODE" >/etc/locale.conf
 printf "KEYMAP=%s\n" "$MY_KEYMAP" >/etc/vconsole.conf
 
-# Host stuff
+# Hostname
 printf '%s\n' "$MY_HOSTNAME" >/etc/hostname
-[ "$MY_INIT" = "openrc" ] && printf 'hostname="%s"\n' "$MY_HOSTNAME" >/etc/conf.d/hostname
-printf "\n127.0.0.1\tlocalhost\n::1\t\tlocalhost\n127.0.1.1\t%s.localdomain\t%s\n" "$MY_HOSTNAME" "$MY_HOSTNAME" >/etc/hosts
 
-# Install boot loader
+if [ "$MY_INIT" = "openrc" ]; then
+	printf 'hostname="%s"\n' "$MY_HOSTNAME" >/etc/conf.d/hostname
+fi
+
+printf "\n127.0.0.1\tlocalhost\n::1\t\tlocalhost\n127.0.1.1\t%s.localdomain\t%s\n" \
+	"$MY_HOSTNAME" "$MY_HOSTNAME" >/etc/hosts
+
+# Configure GRUB
 root_uuid=$(blkid "$PART2" -o value -s UUID)
 
 if [ "$ENCRYPTED" = "y" ]; then
-	my_params="cryptdevice=UUID=$root_uuid:root root=\/dev\/mapper\/root"
+	my_params="cryptdevice=UUID=$root_uuid:root root=/dev/mapper/root"
+else
+	my_params=""
 fi
 
-sed -i "s/^GRUB_CMDLINE_LINUX_DEFAULT.*$/GRUB_CMDLINE_LINUX_DEFAULT=\"$my_params\"/g" /etc/default/grub
-[ "$ENCRYPTED" = "y" ] && sed -i '/GRUB_ENABLE_CRYPTODISK=y/s/^#//g' /etc/default/grub
+# Btrfs root subvolume
+if [ "$MY_FS" = "btrfs" ]; then
+	if [ -n "$my_params" ]; then
+		my_params="$my_params rootflags=subvol=root"
+	else
+		my_params="rootflags=subvol=root"
+	fi
+fi
 
-grub-install --target=x86_64-efi --efi-directory=/boot --recheck
-grub-install --target=x86_64-efi --efi-directory=/boot --removable --recheck
+sed -i \
+	"s|^GRUB_CMDLINE_LINUX_DEFAULT.*$|GRUB_CMDLINE_LINUX_DEFAULT=\"$my_params\"|g" \
+	/etc/default/grub
+
+# Install UEFI GRUB
+grub-install \
+	--target=x86_64-efi \
+	--efi-directory=/boot \
+	--recheck
+
+grub-install \
+	--target=x86_64-efi \
+	--efi-directory=/boot \
+	--removable \
+	--recheck
+
 grub-mkconfig -o /boot/grub/grub.cfg
 
-# Root user
-yes "$ROOT_PASSWORD" | passwd
+# Root password
+printf 'root:%s\n' "$ROOT_PASSWORD" | chpasswd
 
+# Enable wheel sudo permissions
 sed -i '/%wheel ALL=(ALL) ALL/s/^#//g' /etc/sudoers
 
-# Other stuff you should do
+# Configure networking and swap
 if [ "$MY_INIT" = "openrc" ]; then
 	sed -i '/rc_need="localmount"/s/^#//g' /etc/conf.d/swap
 	rc-update add connmand default
+
 elif [ "$MY_INIT" = "dinit" ]; then
-	ln -s /etc/dinit.d/connmand /etc/dinit.d/boot.d/
+	ln -sf /etc/dinit.d/connmand /etc/dinit.d/boot.d/
 fi
 
 # Configure mkinitcpio
-[ "$MY_FS" = "btrfs" ] && sed -i 's/BINARIES=()/BINARIES=(\/usr\/bin\/btrfs)/g' /etc/mkinitcpio.conf
-if [ "$ENCRYPTED" = "y" ]; then
-	sed -i 's/^HOOKS.*$/HOOKS=(base udev autodetect keyboard keymap modconf block encrypt filesystems fsck)/g' /etc/mkinitcpio.conf
-else
-	sed -i 's/^HOOKS.*$/HOOKS=(base udev autodetect keyboard keymap modconf block filesystems fsck)/g' /etc/mkinitcpio.conf
+if [ "$MY_FS" = "btrfs" ]; then
+	sed -i \
+		's|^BINARIES=.*$|BINARIES=(/usr/bin/btrfs)|' \
+		/etc/mkinitcpio.conf
 fi
 
+if [ "$ENCRYPTED" = "y" ]; then
+	sed -i \
+		's|^HOOKS=.*$|HOOKS=(base udev autodetect keyboard keymap modconf block encrypt filesystems fsck)|' \
+		/etc/mkinitcpio.conf
+else
+	sed -i \
+		's|^HOOKS=.*$|HOOKS=(base udev autodetect keyboard keymap modconf block filesystems fsck)|' \
+		/etc/mkinitcpio.conf
+fi
+
+# Regenerate initramfs
 mkinitcpio -P
