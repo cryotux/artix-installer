16587ec99a9c785990c86f33e4bbacfb4bca6878
cryotux <323978812+cryotux@users.noreply.github.com>
2026-09-02 09:48:52 +0000

    Fix of installer.sh

--- src/installer.sh
+++ src/installer.sh
@@ -12,19 +12,25 @@
 # (at your option) any later version.
 #
 # artix-installer is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-# General Public License for more details.
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+# for more details.
 #
 # You should have received a copy of the GNU General Public License
 # along with artix-installer. If not, see <https://www.gnu.org/licenses/>.
 
+# Packages
 pkgs="base base-devel $MY_INIT elogind-$MY_INIT efibootmgr grub dhcpcd wpa_supplicant connman-$MY_INIT"
+
 [ "$MY_FS" = "btrfs" ] && pkgs="$pkgs btrfs-progs"
 [ "$ENCRYPTED" = "y" ] && pkgs="$pkgs cryptsetup cryptsetup-$MY_INIT"
 
 # Partition disk
 printf "label: gpt\n,550M,U\n,,\n" | sfdisk "$MY_DISK"
+
+# Give the kernel time to recognize the new partition table
+sync
+sleep 1
 
 # Format and mount partitions
 if [ "$ENCRYPTED" = "y" ]; then
@@ -32,59 +38,77 @@ if [ "$ENCRYPTED" = "y" ]; then
 	yes "$CRYPTPASS" | cryptsetup open "$PART2" root
 fi
 
+# EFI System Partition
 mkfs.fat -F 32 "$PART1"
 
 if [ "$MY_FS" = "ext4" ]; then
+	# Format root filesystem
 	yes | mkfs.ext4 "$MY_ROOT"
 	mount "$MY_ROOT" /mnt
 
 	# Create swapfile
-	mkdir /mnt/swap
-	fallocate -l "$SWAP_SIZE"G /mnt/swap/swapfile
+	mkdir -p /mnt/swap
+	fallocate -l "${SWAP_SIZE}G" /mnt/swap/swapfile
 	chmod 600 /mnt/swap/swapfile
 	mkswap /mnt/swap/swapfile
+
 elif [ "$MY_FS" = "btrfs" ]; then
+	# Format root filesystem
 	mkfs.btrfs -f "$MY_ROOT"
 
-	# Create subvolumes
+	# Temporarily mount filesystem to create subvolumes
 	mount "$MY_ROOT" /mnt
+
 	btrfs subvolume create /mnt/root
 	btrfs subvolume create /mnt/home
 	btrfs subvolume create /mnt/swap
+
 	umount -R /mnt
 
-	# Mount subvolumes
+	# Mount root subvolume
 	mount -t btrfs -o compress=zstd,subvol=root "$MY_ROOT" /mnt
-	mkdir /mnt/home
-	mkdir /mnt/swap
+
+	# Mount home subvolume
+	mkdir -p /mnt/home
 	mount -t btrfs -o compress=zstd,subvol=home "$MY_ROOT" /mnt/home
+
+	# Mount swap subvolume
+	mkdir -p /mnt/swap
 	mount -t btrfs -o noatime,nodatacow,subvol=swap "$MY_ROOT" /mnt/swap
 
-	# Create swapfile
-	btrfs filesystem mkswapfile -s "$SWAP_SIZE"G /mnt/swap/swapfile
+	# Create Btrfs swapfile
+	btrfs filesystem mkswapfile -s "${SWAP_SIZE}G" /mnt/swap/swapfile
 fi
 
+# Enable swap
 swapon /mnt/swap/swapfile
 
-mkdir /mnt/boot
+# Mount EFI partition
+mkdir -p /mnt/boot
 mount "$PART1" /mnt/boot
 
-case $(grep vendor /proc/cpuinfo) in
-*"Intel"*)
-	pkgs="$pkgs intel-ucode"
-	;;
-*"Amd"*)
-	pkgs="$pkgs amd-ucode"
-	;;
+# CPU microcode
+case "$(grep -m1 'vendor_id' /proc/cpuinfo)" in
+	*GenuineIntel*)
+		pkgs="$pkgs intel-ucode"
+		;;
+	*AuthenticAMD*)
+		pkgs="$pkgs amd-ucode"
+		;;
 esac
 
+# Convert package list into positional arguments
 unset --
 IFS=" "
 for pkg in $pkgs; do
 	set -- "$@" "$pkg"
 done
 
-# Install base system and kernel
+# Install base system
 basestrap /mnt "$@"
+
+# Install kernel and required firmware/tools
 basestrap /mnt linux linux-firmware linux-headers mkinitcpio
+
+# Generate fstab
 fstabgen -U /mnt >/mnt/etc/fstab
